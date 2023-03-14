6c4a9fba07c88efa7c7bac5afe2ed8f38844ccc7
Yuriy-Dobryi <yuriy.dobryi@gmail.com>
2023-03-15 00:14:01 +0200

    i hope it's okay

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`).join('');
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// onclick - встроєний метод бібліотеки basicLightbox
+galleryRef.onclick = (e) => {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+  
+  const origImageSrc = e.target.dataset.source;
+  const origImageMarkup = `<img src='${origImageSrc}' style="width: 1400px; height: 900px;">`;
+
+  const modalWithOrigImage = basicLightbox.create(origImageMarkup, {
+    onShow: () => document.addEventListener("keydown", onEscapeClick),
+  });
+
+  const onEscapeClick = ({ code }) => {
+    if (code === "Escape") {
+      modalWithOrigImage.close();
+      document.removeEventListener("keydown", onEscapeClick);
+    }
+  };
+  
+  modalWithOrigImage.show();
+};
